24e014afea6fab2ba6a2581387bc1f7766270439
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-03-11 22:01:26 +0200

    add packages

--- source/elements/forms/buttons.js
+++ source/elements/forms/buttons.js
@@ -3,8 +3,8 @@
 	$('body').on('mousemove', '.button.is-bg-move, button.is-bg-move', (e) => {
 		let button = e.target;
 
-		const x = e.pageX - button.offsetLeft;
-		const y = e.pageY - button.offsetTop;
+		const x = e.pageX - $(button).offset().left;
+		const y = e.pageY - $(button).offset().top;
 
 			$(button).css({'--x': x+'px', '--y': y+'px' });
 	});
