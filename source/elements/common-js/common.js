42dbafcaa09a7d96fa1a474452bbf3413676deee
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-03-13 17:17:49 +0200

    designer edits

--- source/elements/common-js/common.js
+++ source/elements/common-js/common.js
@@ -2,16 +2,16 @@
 	$('.clipboard_copy').tooltip({
 		disabled: true,
 		position: {
-			my: "center top+10",
-			at: "center bottom"
+			my: 'center top+10',
+			at: 'center bottom'
 		},
 		show: {
-			effect: "drop",
-			direction: "down"
+			effect: 'drop',
+			direction: 'down'
 		},
 		hide: {
-			effect: "drop",
-			direction: "down"
+			effect: 'drop',
+			direction: 'down'
 		},
 		close: function() {
 			$(this).tooltip('disable');
@@ -22,5 +22,27 @@
 	clipboard.on('success', function(e) {
 			$('.clipboard_copy').tooltip('enable').tooltip('open');
 		});
+
+	$('.share-buttons').jsSocials({
+		url: 'https://github.com/OleksandrPavlyshch/arozon',
+		text: 'Arozon',
+		shareIn: 'popup',
+		showLabel: false,
+		showCount: 'inside',
+		shares: [
+			{
+				share: 'facebook',
+				logo: 'img/facebook.svg'
+			},
+			{
+				share: 'twitter',
+				logo: 'img/twitter.svg'
+			},
+			{
+				share: 'telegram',
+				logo: 'img/telegram.svg'
+			}
+		]
+	});
 
 })(jQuery);
--- source/elements/forms/buttons.js
+++ source/elements/forms/buttons.js
@@ -1,12 +1,12 @@
-(($) => {
+// (($) => {
 
-	$('body').on('mousemove', '.button.is-bg-move, button.is-bg-move', (e) => {
-		let button = e.target;
+// 	$('body').on('mousemove', '.button.is-bg-move, button.is-bg-move', (e) => {
+// 		let button = e.target;
 
-		const x = e.pageX - $(button).offset().left;
-		const y = e.pageY - $(button).offset().top;
+// 		const x = e.pageX - $(button).offset().left;
+// 		const y = e.pageY - $(button).offset().top;
 
-			$(button).css({'--x': x+'px', '--y': y+'px' });
-	});
+// 			$(button).css({'--x': x+'px', '--y': y+'px' });
+// 	});
 
-})(jQuery);
+// })(jQuery);
