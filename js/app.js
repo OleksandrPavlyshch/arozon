d7634cf33f1712ad93a04abebfd767126cdd83f7
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-04-05 21:57:13 +0300

    Update 2018-04-05T18:56:54.892Z

--- js/app.js
+++ js/app.js
@@ -210,46 +210,6 @@
 
 })(jQuery);
 
-(($) => {
-	let initHeader = () => {
-		$(window).scroll(function() {
-			let _top = parseInt($(window).height() / 2)
-				, _scroll = parseInt($(window).height() / 3);
-
-			if ($(window).scrollTop() >= _scroll ) {
-				$('#header').addClass('is-scroll');
-				$('.select_city').removeClass('open');
-			} else {
-				$('#header').removeClass('is-scroll');
-			}
-
-			if ($(window).scrollTop() >= _top) {
-				$('#header').addClass('is-fixed');
-			} else {
-				$('#header').removeClass('is-fixed');
-			}
-		});
-	};
-
-	initHeader();
-
-	//init menu open
-	let $body = $('body')
-		, $menuButton = $('.toggle-menu-button')
-		, menuShowClass = 'is-menu-show';
-
-	$menuButton.on('click', function() {
-		$body.toggleClass(menuShowClass);
-	});
-
-
-	// $(document).click( function(event){
-	// 	if( $(event.target).closest(menuClass).length )
-	// 		return;
-	// 	$body.removeClass(menuShowClass);
-	// });
-
-})(jQuery);
 // (($) => {
 
 // 	$('body').on('mousemove', '.button.is-bg-move, button.is-bg-move', (e) => {
@@ -506,8 +466,49 @@ let setTimepickerValue = ($timepicker, data) => {
 (($) => {
 	$('.select-custom').niceSelect();
 })(jQuery);
+(($) => {
+	let initHeader = () => {
+		$(window).scroll(function() {
+			let _top = parseInt($(window).height() / 2)
+				, _scroll = parseInt($(window).height() / 3);
+
+			if ($(window).scrollTop() >= _scroll ) {
+				$('#header').addClass('is-scroll');
+				$('.select_city').removeClass('open');
+			} else {
+				$('#header').removeClass('is-scroll');
+			}
+
+			if ($(window).scrollTop() >= _top) {
+				$('#header').addClass('is-fixed');
+			} else {
+				$('#header').removeClass('is-fixed');
+			}
+		});
+	};
+
+	initHeader();
+
+	//init menu open
+	let $body = $('body')
+		, $menuButton = $('.toggle-menu-button')
+		, menuShowClass = 'is-menu-show';
+
+	$menuButton.on('click', function() {
+		$body.toggleClass(menuShowClass);
+	});
+
+
+	$(document).click( function(event){
+		if( $(event.target).closest('.header').length )
+			return;
+		$body.removeClass(menuShowClass);
+	});
+
+})(jQuery);
+
+
 
 (($) => {
 	$( "#calc-forms" ).tabs();
 })(jQuery);
-
