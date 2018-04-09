4deaa0956f83d53e3c7fadbf0e3abacf964de71c
OleksandrPavlyshch <lokdawn80@gmail.com>
2018-04-09 05:28:57 +0300

    add calculateCost function

--- source/elements/calc_stepper/script.js
+++ source/elements/calc_stepper/script.js
@@ -19,17 +19,19 @@
 				, defaultText = 'пропустить'
 				, nextText = 'далее';
 
-					console.log($input.prop('checked'));
+					// console.log($input.prop('checked'));
 				if( $input.prop('checked') === false && $this.hasClass('calc_stepper-package-default') === false ){
 					$stepInputs.prop('checked', false);
 					$input.prop('checked', 'checked');
 					$nextButton.text(nextText);
+					calculateCost();
 					return;
 				}
 
 				$nextButton.text(defaultText);
 				$stepInputs.prop('checked', false);
 				$defaultInput.prop('checked', 'checked');
+				calculateCost();
 
 		});
 
@@ -84,30 +86,99 @@
 			}, 200));
 		}
 
-		let $costOutput =  $('.calc_stepper-sum_block-price')
+		let $costOutput =  $('.calc_stepper-sum_block-price');
 
-		let s = 111;
-		let calculateCost = (formValues) => {
+		let calculateCost = () => {
 			let config = {
-
+				'base-price' : 40
+				, 'package': {
+					'baby': 1.37
+					, 'pet': 1.37
+					, 'mans': 1.37
+					, 'party': 1.37
+				}
+				, 'place-type': {
+					'apartment': 1
+					, 'house': 1.15
+					, 'office': 1
+					, 'boutique': 1
+				}
+				, 'clean-type': {
+					'support': 1
+					, 'general': 1
+					, 'after-construction': 1.27
+				}
+				, 'refrigerator': 100
+				, 'microwave': 40
+				, 'сhandelier': 0
+				, 'oven': 180
+				, 'wash-dishes': 0
+				, 'dry-cleaning': 0
+				, 'window': 150
+				, 'balcony': 200
 			};
 
-			console.table( $stepperForm.serializeArray());
+			let formValues = formValueArrayToObj($stepperForm.serializeArray());
 
-			++s
-			$costOutput.text(s);
-		}
 
-		$stepper.on('change', 'input', function(event) {
+			let result = config['base-price'];
+
+				result *= config.package[formValues.package] || 1;
+				result *= config['place-type'][formValues['place-type']] || 1;
+				result *= formValues['square-amount'] || 1;
+				result *= config['clean-type'][formValues['clean-type']] || 1;
+
+				if(formValues.refrigerator) {
+					result += config.refrigerator;
+				}
+
+				if(formValues.microwave) {
+					result += config.microwave;
+				}
+
+				if(formValues.сhandelier) {
+					result += config.сhandelier;
+				}
+
+				if(formValues.oven) {
+					result += config.oven;
+				}
+
+				if(formValues['wash-dishes']) {
+					result += config['wash-dishes'];
+				}
+
+				if(formValues['dry-cleaning']) {
+					result += config['dry-cleaning'];
+				}
+
+				if(formValues.window) {
+					result += (config.window * formValues['window-amount']);
+				}
+
+				if(formValues.balcony) {
+					result += (config.balcony * formValues['balcony-amount']);
+				}
+
+			// console.table( formValueArrayToObj($stepperForm.serializeArray()));
+
+			$costOutput.text(Math.round(result));
+		};
+
+		$stepper.on('change', 'input', function() {
 
 			calculateCost();
 		});
 
-		$stepper.on('step4', function(e){
+		$stepper.on('stepchange', function(){
+			calculateCost();
+		});
+
+		$stepper.on('step4', function(){
 			$calcStepperBlock.fadeOut();
 		});
 
-		$stepper.on('step5', function(e){
+		$stepper.on('step5', function(){
 			$calcStepperBlock.fadeIn();
 		});
 
